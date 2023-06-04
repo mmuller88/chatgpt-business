f018aef743ec1140d92b595a55fd62d966070bcf
Martin Mueller <damadden88@googlemail.com>
2023-06-04 07:23:02 +0100

    join prompts

--- src/index.ts
+++ src/index.ts
@@ -31,24 +31,34 @@ const prompt = async () => {
     {
       role: 'system',
       content: `
-      You are a system to extract the most important information.
-      You can only answer using the json format. Care to not include any trailing commas.
-      You can not write any text outside of the brackets of the json. If you have anything to add include your thoughts to the comment field inside the json object.
+        You are a system to extract the most important information.
+        You can only answer using the json format. Care to not include any trailing commas.
+        You can not write any text outside of the brackets of the json. If you have anything to add include your thoughts to the comment field inside the json object.
       `,
     },
     {
       role: 'user',
       content: `
-      These are the URLs you are about to assess: ${JSON.stringify(urls)}
-        Order the following URLs by the amount of information they might convey, e.g. URLs of contact pages, privacy policies or about pages are more important, you can limit the most_promising_urls list up.
-        Please punish external urls, like social media with lower importance.
+        List IT companies from Lisbon.
         An example would look like this: ${JSON.stringify({
-          most_promising_urls: ['https://mywebsite.com/imprint'],
-          comment: 'The /imprint page sounds promising',
+          url: ['https://mywebsite.pt'],
         })}.
-        The most_promising_urls key is an array of strings, not objects.
+        The url key is an array of strings, not objects.
       `,
     },
+    // {
+    //   role: 'user',
+    //   content: `
+    //     These are the URLs you are about to assess: ${JSON.stringify(urls)}
+    //     Order the following URLs by the amount of information they might convey, e.g. URLs of contact pages, privacy policies or about pages are more important, you can limit the most_promising_urls list up.
+    //     Please punish external urls, like social media with lower importance.
+    //     An example would look like this: ${JSON.stringify({
+    //       most_promising_urls: ['https://mywebsite.com/imprint'],
+    //       comment: 'The /imprint page sounds promising',
+    //     })}.
+    //     The most_promising_urls key is an array of strings, not objects.
+    //   `,
+    // },
   ];
 
   const result = await openai.createChatCompletion({
